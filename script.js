0248194ad92670ea8a0fac701d4f0bcc83a926d1
triangletrianglesquareplusx <69000271+triangletrianglesquareplusx@users.noreply.github.com>
2022-03-11 13:39:12 +0200

    changing styles for players

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let currentScore = 0;
 let activePlayer = 0;
 const playerScores = [0,0];
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const diceElement = document.querySelector('.dice');
 const playerZeroScoreElement = document.querySelector('#score--0');
 const playerOneScoreElement = document.querySelector('#score--1');
@@ -46,6 +49,8 @@ btnRollElement.addEventListener('click', ()=>{
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         activePlayer = activePlayer === 0 ? 1:0;
         currentScore = 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
         
     }
     
